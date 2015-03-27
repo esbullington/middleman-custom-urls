d5dc28ed4084bba8a3e9e06eda6ac69fd420e2cc
mattjorn <mattjorn@users.noreply.github.com>
2015-03-27 16:43:28 +0100

    Remove debug messages

--- lib/middleman-custom-urls/extension.rb
+++ lib/middleman-custom-urls/extension.rb
@@ -11,7 +11,6 @@ module Middleman
             extname = File.extname(orig_path)
             orig_path = orig_path.chomp(extname)
           end
-          puts orig_path
           new_path = resource.raw_data[:custom_url]
           resource.destination_path = resource.destination_path.gsub(orig_path, new_path)
         end
