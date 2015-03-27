e9620c597169103f9ca5bf733a3222c695bf0221
mattjorn <mattjorn@users.noreply.github.com>
2015-03-27 16:39:32 +0100

    Change calculation of custom path to allow this plugin to work with middleman-blog's pagination

--- lib/middleman-custom-urls/extension.rb
+++ lib/middleman-custom-urls/extension.rb
@@ -5,7 +5,13 @@ module Middleman
         resources.each do |resource|
           next unless resource.raw_data[:custom_url].present?
 
-          orig_path = resource.path.chomp(File.extname(resource.path))
+          orig_path = resource.source_file.gsub(app.source_dir + '/', '')
+          extname = File.extname(resource.source_file)
+          while extname != ''
+            extname = File.extname(orig_path)
+            orig_path = orig_path.chomp(extname)
+          end
+          puts orig_path
           new_path = resource.raw_data[:custom_url]
           resource.destination_path = resource.destination_path.gsub(orig_path, new_path)
         end
